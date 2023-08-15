e7c0d25c4a2d4069eeb37e52673f2d62b2f2ac24
Celeste <cnyx7@proton.me>
2023-08-15 16:29:35 +0000

    first commit ex1-15

--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,8 @@
 import random
 
 # your code here
+def generate_random():
+    randomNum = random.randint(0, 9)
+    return randomNum
+
+print(generate_random())
--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,13 @@
 def fizz_buzz():
     # your code here
-
+    for number in range(1, 101):
+        if (number % 3 == 0 and number % 5 == 0):
+            print('FizzBuzz')
+        elif (number % 3 == 0):
+            print('Fizz')
+        elif (number % 5 == 0):
+            print('Buzz')
+        else:
+            print(number)
 
 fizz_buzz()
